710caada24622815feb38be9069e870a449cc6a1
David05 <davidgreendevlops@gmail.com>
2025-01-04 19:48:48 +0100

    playing with stuffs

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    char oneLetterCharacter = 'g';
+    char oneLetterCharacter = 66;
     byte oneTwentyEight = 126;
     System.out.println(oneTwentyEight);
     System.out.print(oneLetterCharacter);
