8143eed971b0f05dc5555bf1dd52974174071675
David05 <davidgreendevlops@gmail.com>
2025-01-05 20:10:22 +0100

    playing with stuffs

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ public class Main {
   public static void main(String[] args) {
     char oneLetterCharacter = 66;
     String aSringOfRandomCharacters = "dhdhdhdd";
+    float justAFloatingNumber = 12.8f;
+    // float justAFloatingNumber = 12.8f;
     byte oneTwentyEight = 126;
     System.out.println(oneTwentyEight);
     System.out.println(aSringOfRandomCharacters);
