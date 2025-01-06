777d19ce60dcb5b927bd70d09f3d2dbd5207c837
David05 <davidgreendevlops@gmail.com>
2025-01-06 12:48:30 +0100

    playing with stuffs

--- Main.java
+++ Main.java
@@ -4,10 +4,12 @@ public class Main {
     String aSringOfRandomCharacters = "dhdhdhdd";
     float justARandomFloatingNumber = 12.8f;
     double justARandomDoubleNumber = 12.77728d;
+    char castedStringOfRandomCharacters = aSringOfRandomCharacters.charAt(1);
     byte oneTwentyEight = 126;
     System.out.println(oneTwentyEight);
     System.out.println(aSringOfRandomCharacters);
     System.out.println(justARandomFloatingNumber);
+    System.out.println(castedStringOfRandomCharacters);
     System.out.println(justARandomDoubleNumber);
     System.out.print(oneLetterCharacter);
   }
